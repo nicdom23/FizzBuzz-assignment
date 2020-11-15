4370163c49c49f5112eb884fd07b24e240f7f679
Domenis Nicola S262688 <nicoladomenis23@hotmail.it>
2020-11-15 14:07:02 +0100

    Completed the code, created correct .jar file ( I presume) and created .zip distribution

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,23 @@ package dssc.assignment.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String... args) {
-        
+        for(int i = 0; i< 106 ; i++) {
+            String s = "";
+            if (i % 5 != 0 && i % 3 != 0 && i % 7 != 0)
+                System.out.println(i);
+            if (i % 3 == 0) {
+                s = s + "Fizz";
+            }
+            if (i % 5 == 0) {
+                s = s + "Buzz";
+            }
+            if (i % 7 == 0) {
+                s = s + "Bang";
+            }
+            if(s.compareTo("") >0)
+            System.out.println(s);
+        }
+
     }
 
 }
